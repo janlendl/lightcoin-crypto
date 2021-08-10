cb2273cd39bea6a086d055519c1e361303e0a620
Lendl Peralta <janlendl@gmail.com>
2021-08-10 20:08:58 +0000

    Created Class transaction as as superclass to Withdrawal and Deposit. Created a get value to W and D class then moved commit to Transation class

--- index.js
+++ index.js
@@ -16,22 +16,33 @@ class Transaction {
     this.amount = amount;
     this.account = account;
   }
+
+  commit() {
+    this.account.balance += this.value;
+  }
 }
 
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
-  }
+  // commit() {
+  //   this.account.balance -= this.amount;
+  // }
 
+  get value() {
+    return -this.amount;
+  }
 }
 
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  // commit() {
+  //   this.account.balance += this.amount;
+  // }
+
+  get value() {
+    return this.amount;
   }
 }
 
@@ -43,33 +54,17 @@ class Deposit extends Transaction {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-const myAccount = new Account('snow-patrol');
+const myAccount = new Account('billybob');
 
-console.log('New Account Created: ', myAccount.username);
+console.log('Starting Balance:', myAccount.balance);
 
-t1 = new Deposit(50.25, myAccount);
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-
-Account2 = new Account('Lendl');
-t4 = new Deposit(400, Account2);
-t4.commit();
-console.log('Transaction 4', t4);
-
-t5 = new Withdrawal(150, Account2)
-t5.commit();
-
-console.log(Account2);
-console.log(myAccount)
+console.log('Ending Balance:', myAccount.balance);
 
 
 
